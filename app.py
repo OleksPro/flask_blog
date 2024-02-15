f8734d57008aa2f645de534cc0fb29f41c903872
OlexPro <oleks.prokopenko92@gmail.com>
2024-02-15 14:05:44 +0200

    created page for added articles

--- app.py
+++ app.py
@@ -29,6 +29,18 @@ def about():
     return render_template("about.html")
 
 
+@app.route('/posts')
+def posts():
+    articles = Article.query.order_by(Article.date.desc()).all()
+    return render_template("posts.html", articles=articles)
+
+
+@app.route('/posts/<int:id>')
+def post_detail(id):
+    article = Article.query.get(id)
+    return render_template("post_detail.html", article=article)
+
+
 @app.route('/create_article', methods=['POST', 'GET'])
 def create_article():
     if request.method == 'POST':
@@ -41,7 +53,7 @@ def create_article():
         try:
             db.session.add(article)
             db.session.commit()
-            return redirect('/')
+            return redirect('/posts')
         except:
             return "An error occurred while adding an article"
     else:
